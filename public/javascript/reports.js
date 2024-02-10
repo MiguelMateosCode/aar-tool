4ef222d85c09b85a09afe600ed4b7e48b508767a
MiguelMateosCode <159456904+MiguelMateosCode@users.noreply.github.com>
2024-02-10 17:06:55 +0100

    Update reports.js

--- public/javascript/reports.js
+++ public/javascript/reports.js
@@ -2,31 +2,42 @@ function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.querySelector("table");
     switching = true;
+    // Set the sorting direction to ascending:
     dir = "asc";
+    // Make a loop that will continue until no switching has been done:
     while (switching) {
+        // Start by saying: no switching is done:
         switching = false;
         rows = table.rows;
+        // Loop through all table rows (except the first, which contains table headers):
         for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
             shouldSwitch = false;
+            // Get the two elements you want to compare, one from current row and one from the next:
             x = rows[i].getElementsByTagName("TD")[n];
             y = rows[i + 1].getElementsByTagName("TD")[n];
+            // Check if the two rows should switch place, based on the direction, asc or desc:
             if (dir == "asc") {
                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
                     shouldSwitch = true;
                     break;
                 }
             } else if (dir == "desc") {
                 if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                    shouldSwitch= true;
+                    shouldSwitch = true;
                     break;
                 }
             }
         }
         if (shouldSwitch) {
+            // If a switch has been marked, make the switch and mark that a switch has been done:
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
+            // Each time a switch is done, increase this count by 1:
             switchcount++;      
         } else {
+            // If no switching has been done AND the direction is "asc", set the direction to "desc" and run the while loop again.
             if (switchcount == 0 && dir == "asc") {
                 dir = "desc";
                 switching = true;
@@ -37,21 +48,22 @@ function sortTable(n) {
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("searchInput").addEventListener("input", function() {
-        var input, filter, table, tr, td, i, txtValue;
+        var input, filter, table, tr, i, tdName, txtValueName;
         input = document.getElementById("searchInput");
         filter = input.value.toUpperCase();
         table = document.querySelector("table");
         tr = table.getElementsByTagName("tr");
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[2];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+
+        for (i = 1; i < tr.length; i++) {
+            tdName = tr[i].getElementsByTagName("td")[1]; // Se enfoca en la columna 'Name'
+            if (tdName) {
+                txtValueName = tdName.textContent || tdName.innerText;
+                if (txtValueName.toUpperCase().indexOf(filter) > -1) {
                     tr[i].style.display = "";
                 } else {
                     tr[i].style.display = "none";
                 }
-            }       
+            }
         }
     });
 });
